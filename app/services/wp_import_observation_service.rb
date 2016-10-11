2e9ca028f5c29c962695b2eb5be05f60c77c906a
yukiya <yukiya.ueda@gmail.com>
2016-10-12 00:14:50 +0900

    観察データインポート＜プロトタイプ

---
+++ app/services/wp_import_observation_service.rb
@@ -0,0 +1,45 @@
+require 'nokogiri'
+class WpImportObservationService
+  def self.call
+    puts "start..."
+
+    file = File.read('doc/wordpress.xml')
+    xml = Nokogiri::XML(file)
+
+    item_nodes = xml.xpath('//item')
+    item_nodes.each_with_index do |item, i|
+      puts title =item.xpath('title').text
+      ob = Observation.find_or_initialize_by(title: title)
+      ob.description = item.xpath('description').text
+      ob.link = item.xpath('link').text
+      ob.pub_date = item.xpath('pubDate').text.to_datetime
+      creator = item.xpath('dc:creator').text
+      puts '--user--'
+      puts creator
+      ob.user = User.find_by(wp_author_login: creator)
+
+      ob.wp_post_id = item.xpath('post_id').text
+      ob.posted_at = item.xpath('post_date').text.to_datetime
+
+
+      meta_nodes = item.xpath('wp:postmeta')
+      cols = {}
+      # values = meta_nodes.map do |node|
+      meta_nodes.each do |node|
+        key = node.xpath('wp:meta_key').text
+        # 読み飛ばし属性
+        unless key =~ /^_/ || key == 'i'
+          value = node.xpath('wp:meta_value').text
+          cols[key] = value
+          # node.xpath('wp:meta_value').text
+        end
+      end
+      if ob.valid?
+        ob.save
+      else
+        puts ob.errors.messages
+      end
+    end
+    puts "complete! See intro.txt."
+  end
+end
