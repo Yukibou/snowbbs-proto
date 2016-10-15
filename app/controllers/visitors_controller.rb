ca88696d9a8b1da2cecb3d156058cddd9c11bb85
yukiya <yukiya.ueda@gmail.com>
2016-10-16 00:24:52 +0900

    列修正

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,7 @@
 class VisitorsController < ApplicationController
   def index
     @users = User.joins(:observations).group('users.id').order('count(users.id) DESC').limit 10
-    @range = Observation.group('category').order('count(category) DESC').count
+    @areas = Area.joins(:observations).group('areas.id').order('count(areas.id) DESC')
+    # @range = Observation.group('category').order('count(category) DESC').count
   end
 end
