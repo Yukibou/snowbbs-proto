016078b08a584332b765b3df153d76cf2583d722
yukiya <yukiya.ueda@gmail.com>
2016-10-19 00:43:27 +0900

    split elevation

--- Gemfile
+++ Gemfile
@@ -8,8 +8,13 @@ gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'moji'
 group :development, :test do
   gem 'byebug', platform: :mri
+  gem 'pry-rails'  # rails console(もしくは、rails c)でirbの代わりにpryを使われる
+  gem 'pry-doc'    # methodを表示
+  gem 'pry-byebug' # デバッグを実施(Ruby 2.0以降で動作する)
+  gem 'pry-stack_explorer' # スタックをたどれる
 end
 group :development do
   gem 'web-console'
---
+++ app/services/update_observation_service.rb
@@ -0,0 +1,14 @@
+class UpdateObservationService
+  def split_elevation
+    puts 'start..'
+    Observation.all.each do |ob|
+      puts '---------------'
+      puts elv = ob.elevation.strip.delete("@mｍ")
+      elvs = elv.split(/[-,～,~,−,ー]/)
+      puts ob.elevation_low = Moji.zen_to_han(elvs[0]).to_i if elvs[0].present?
+      puts ob.elevation_high = Moji.zen_to_han(elvs[1]).to_i if elvs[1].present?
+      ob.save!
+    end
+    puts 'end.'
+  end
+end
