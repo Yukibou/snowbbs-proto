7b3311ad009734ec8b334f018e32d9d0a2cea847
yukiya <yukiya.ueda@gmail.com>
2016-10-19 01:01:58 +0900

    split 文字を追加

--- app/services/update_observation_service.rb
+++ app/services/update_observation_service.rb
@@ -4,9 +4,11 @@ class UpdateObservationService
     Observation.all.each do |ob|
       puts '---------------'
       puts elv = ob.elevation.strip.delete("@mｍ")
-      elvs = elv.split(/[-,～,~,−,ー]/)
-      ob.elevation_low = Moji.zen_to_han(elvs[0]).to_i if elvs[0].present?
-      ob.elevation_high = Moji.zen_to_han(elvs[1]).to_i if elvs[1].present?
+      elvs = elv.split(/[-,～,~,−,ー,〜,－]/)
+      puts low = Moji.zen_to_han(elvs[0]).to_i if elvs[0].present?
+      puts high = Moji.zen_to_han(elvs[1]).to_i if elvs[1].present?
+      ob.elevation_low = low
+      ob.elevation_high = high
       ob.save!
     end
     puts 'end.'
